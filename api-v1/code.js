db3ffe98f6908f2e05d75e0ef074aa32175a48ef
reezpatel <reezpatel@gmail.com>
2017-02-06 15:01:31 +0530

    - Fix Process not Ending

--- api-v1/code.js
+++ api-v1/code.js
@@ -1,20 +1,45 @@
+const exec = require('child_process').exec;
+
 //c files
 var c_code = require("./code/c-code");
 var c_check = require("./code/c-check");
+var cCounter = 0;
 
 //cpp files
 var cpp_code = require("./code/cpp-code");
 var cpp_check = require("./code/cpp-check");
+var cppCounter = 0;
 
 //java files
 var java_code = require("./code/java-code");
 var java_check = require("./code/java-check");
+var javaCounter = 0;
 
 //matlab files
 var matlab_code = require("./code/matlab-code");
 var matlab_check = require("./code/matlab-check");
+var mathsLabCounter = 0;
 
 var fs = require('fs');
+
+var deleteAllProcess = function(subjectID) {
+    console.log(subjectID);
+    var fileName = null;
+    if(subjectID == 0) {
+        fileName = "a.out";
+    }else if(subjectID == 2) {
+        fileName = "java";
+    }else if(subjectID == 3) {
+        fileName = "octave-gui";
+    }
+
+    exec("killall " + fileName,(error,stdout,stderr) => {
+        if(error) {
+            console.log('Error in killing '+ fileName + " with Message: " + error.message);
+        }
+        console.log("Process Killed " + fileName);
+    });
+};
 
 var code = function(reqData,callback) {
 
@@ -50,42 +75,87 @@ var code = function(reqData,callback) {
     }
 
     if(lang == 'c') {
+        cCounter++;
+
+        console.log(cCounter);
+
         if(mode == "verify") {
             c_check.execute(reqData,function(jsonData){
+                cCounter--;
+                if(cCounter == 0) {
+                    deleteAllProcess(0);
+                }
                 callback(jsonData);
             });
         }else {
             c_code.execute(reqData,function(jsonData){
+                cCounter--;
+                if(cCounter == 0) {
+                    deleteAllProcess(0);
+                }
+
                 callback(jsonData);
+
             });
         }
     } else if(lang == "cpp"){
+        cppCounter++;
+
         if(mode == "verify") {
             cpp_check.execute(reqData,function(jsonData){
+                cppCounter--;
+                if(cppCounter == 0) {
+                    deleteAllProcess(0);
+                }
                 callback(jsonData);
             });
         }else {
             cpp_code.execute(reqData,function(jsonData){
+                cppCounter--;
+                if(cppCounter == 0) {
+                    deleteAllProcess(0);
+                }
                 callback(jsonData);
             });
         }    
     }else if(lang == "java") {
+        javaCounter++;
+
+
         if(mode == "verify") {
             java_check.execute(reqData,function(jsonData){
+                javaCounter--;
+                if(javaCounter == 0) {
+                    deleteAllProcess(2);
+                }
                 callback(jsonData);
             });
         }else {
             java_code.execute(reqData,function(jsonData){
+                javaCounter--;
+                if(javaCounter == 0) {
+                    deleteAllProcess(2);
+                }
                 callback(jsonData);
             });
         }       
     }else if(lang == "matlab") {
+        mathsLabCounter++;
+
         if(mode == "verify") {
             matlab_check.execute(reqData,function(jsonData){
+                mathsLabCounter--;
+                if(mathsLabCounter == 0) {
+                    deleteAllProcess(3);
+                }
                 callback(jsonData);
             });
         }else {
             matlab_code.execute(reqData,function(jsonData){
+                mathsLabCounter--;
+                if(mathsLabCounter == 0) {
+                    deleteAllProcess(3);
+                }
                 callback(jsonData);
             });
         }       
